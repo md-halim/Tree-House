7bd395c799c2321c65993cc261757c4b45c44a97
Abdul Halim <mdhalim.sof@gmail.com>
2021-10-07 08:37:49 +0600

    tree house temp

---
+++ js/custome.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    
+    // slik slider
+    
+    $('.slik_slide').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    //wow
+    
+    new WOW().init();
+
+
+
+});
